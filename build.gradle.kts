68c50723f3abc98c9b1bb20927b70ba6cf4d3091
Kyle Lin <minecraft.kyle.train@gmail.com>
2024-06-23 01:22:21 +0800

    Release 2.0.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,13 +24,13 @@ buildscript {
         classpath("net.minecraftforge.gradle:ForgeGradle:5.1.+") {
             isChanging = true
         }
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
     }
 }
 
 plugins {
     java
-    kotlin("jvm") version "1.9.24"
+    kotlin("jvm") version "2.0.0"
     id("net.kyori.blossom") version "1.3.1"
     id("com.github.johnrengelman.shadow") version "7.0.0"
     `maven-publish`
